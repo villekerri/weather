5dcee8a33009703ae30e319ac629ba0ef66ff654
Lauri <lauri.a.uotila@gmail.com>
2020-03-10 12:02:45 +0200

    hmm

--- api/locations.js
+++ api/locations.js
@@ -1,36 +1,72 @@
 var express = require('express');
 var app = express();
-var sql = require('mysql');
+const sql = require('mysql2/promise');
 var util = require("util")
 var path = require("path");
 var bodyParser = require("body-parser");
+const cors = require('cors');
 
 //mysql db
-var con = sql.createConnection({
-    host: 'localhost',
-    user: 'ryhma9',
-    password: 'ryhma9',
-    database: 'weather'
-})
 
 //POSTin jsonia varten
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+async function postLocation(city, address) {
+    let sql_query = "INSERT INTO locations (city,address) VALUES (?, ?)";
+    console.log(sql_query, city, address);
+    const con = await sql.createConnection({
+        host: "localhost",
+        user: "ryhma9",
+        password: "ryhma9",
+        database: "weather"
+      });
+    const result = await con.execute(sql_query, [city, address]);
+    return result;
+}
+async function getLocations() {
+    let sql_query = "SELECT * FROM locations";
+    const con = await sql.createConnection({
+        host: "localhost",
+        user: "ryhma9",
+        password: "ryhma9",
+        database: "weather"
+      });
+    const result = await con.execute(sql_query);
+    return result;
+}
+
+
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../ui', 'index.html'))
+    res.sendFile(path.join(__dirname, '../', 'public/index.html'))
 })
 
-app.get('/', function (req, res, next) {
-    con.query('SELECT * FROM locations', function (err, result) {
-        if (err) {
-            return next(err)
-        }
-        if (result.length === 0) {
-            result = 'tyhjää'
-        }
-        res.send(result)
-    })
+app.get('/locations', function (req, res, next) {
+    console.log(req.query)
+    let result = getLocations();
+
+    result.then( ([rows, fields]) => {
+        console.log(rows, fields);
+        res.send(rows);
+     })
+     .catch(console.log);
+
+
+})
+
+app.post('/post', function (req, res, next) {
+    let city = req.query.city;
+    let address = req.query.address;
+    console.log(req.query)
+    let result = postLocation(city, address);
+
+    result.then( ([rows, fields]) => {
+        console.log(rows, fields);
+        res.sendStatus(200);
+     })
+     .catch(console.log);
 })
 
 //http://127.0.0.1:8081/
